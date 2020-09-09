b79ee34bd371da8d76d7d95c10ccc2274e15acbb
Luiz M. Faria <maltezfaria@gmail.com>
2020-09-09 17:00:20 +0200

    cleanup

--- create_sha.jl
+++
@@ -1,5 +0,0 @@
-using Tar, Inflate, SHA
-
-filename = "gmsh-4.6.0-Linux64-sdk.tgz"
-println("sha256: ", bytes2hex(open(sha256, filename)))
-println("git-tree-sha1: ", Tar.tree_hash(IOBuffer(inflate_gzip(filename))))
