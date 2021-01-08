2bc16f024d34792a35e2568e7b50ef312e2816f8
Luiz M. Faria <maltezfaria@gmail.com>
2021-01-08 13:44:53 +0100

    update tests to actually try and use the gmsh api

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,20 @@ using Test
 using GmshSDK
 
 gmsh.initialize()
+    # try some basic things just to make sure it does not error
+    gmsh.model.geo.addPoint(0,0,0)
+    gmsh.model.occ.addSphere(0,0,0,1)
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.generate(3)
+    @test true == true # gmsh was initialized without errors
+gmsh.finalize()
+
+gmsh.initialize()
+    # try some basic things just to make sure it does not error
+    gmsh.model.geo.addPoint(0,0,0)
+    gmsh.model.occ.addSphere(0,0,0,1)
+    gmsh.model.occ.synchronize()
+    gmsh.model.mesh.generate(3)
+    gmsh.model.mesh.partition(5)
     @test true == true # gmsh was initialized without errors
 gmsh.finalize()
